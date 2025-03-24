e8a8113150c998b911293a14d60451f5b812471b
sebastiansmorales <sebastian@stereum.tech>
2025-03-24 10:25:37 -0400

    fix

--- mobil/home/MovimientosScreen.tsx
+++ mobil/home/MovimientosScreen.tsx
@@ -13,9 +13,9 @@ const fetchPayments = async (): Promise<PaymentItem[]> => {
     return new Promise((resolve) => {
         setTimeout(() => {
             resolve([
-                { idAccount: 1, amount: 100, account: { name: 'Camila', lastName: 'Perez', idAccount: 1 }, date: '20-03-2025' },
-                { idAccount: 2, amount: 200, account: { name: 'Sebastian', lastName: 'Morales', idAccount: 2 }, date: '19-03-2025' },
-                { idAccount: 3, amount: 150, account: { name: 'Camila', lastName: 'Perez', idAccount: 3 }, date: '18-03-2025' },
+                { idAccount: 1, amount: 100, account: { name: 'Camila', lastName: 'Lino', idAccount: 1 }, date: '20-03-2025' },
+                { idAccount: 2, amount: 200, account: { name: 'Santiago', lastName: 'Suarez', idAccount: 2 }, date: '19-03-2025' },
+                { idAccount: 3, amount: 150, account: { name: 'Camila', lastName: 'Lino', idAccount: 3 }, date: '18-03-2025' },
             ]);
         }, 1000);
     });
@@ -44,9 +44,15 @@ export default function MovimientosScreen() {
     const renderItem = ({ item }: { item: PaymentItem }) => (
       <Card style={styles.card}>
           <Card.Content>
-              <Text style={styles.name}>{`${item.account.name} ${item.account.lastName}`}</Text>
-              <Text style={styles.amount}>Monto: {item.amount} Bs</Text>
-              <Text style={styles.date}>Fecha: {item.date}</Text>
+              <View style={{flexDirection:"row" ,justifyContent:"space-between",alignItems:"center"}}>
+                <View>
+                <Text style={styles.name}>{`${item.account.name} ${item.account.lastName}`}</Text>
+           
+           <Text style={styles.date}>Fecha: {item.date}</Text>
+                </View>
+              <Text style={styles.amount}>Monto: <Text style={{fontWeight:600}}>{item.amount} Bs </Text></Text>
+              </View>
+             
           </Card.Content>
       </Card>
     );
